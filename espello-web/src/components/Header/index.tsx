9f57a5b89cd1faebcc736cdbc47e69d5ebd9e0e5
Kanespello <162005722+Kanespello@users.noreply.github.com>
2024-04-16 23:08:54 +0530

    button navigate

--- espello-web/src/components/Header/index.tsx
+++ espello-web/src/components/Header/index.tsx
@@ -1,19 +1,29 @@
 import React from 'react';
+import {useNavigate} from 'react-router-dom';
 import './header.css';
 
 function Header() {
-    return (
-        <div className="header">
-            <div className="header-left">
-                <div className="header-left-logo">Espello.ai</div>
-            </div>
-            <div className="header-right">
-                <div className="header-right-buttons">
-                    <div className="header-right-buttons-login">Login</div>
-                </div>
+
+    const navigate = useNavigate();
+
+    const onClickLoginButton = () => {
+        navigate('/Login');
+    };
+
+    const onClickLogo = () => {
+        navigate('/');
+    };
+
+    return (<div className="header">
+        <div className="header-left">
+            <div className="header-left-logo" onClick={onClickLogo}>Espello.ai</div>
+        </div>
+        <div className="header-right">
+            <div className="header-right-buttons" onClick={onClickLoginButton}>
+                <div className="header-right-buttons-login">Login</div>
             </div>
         </div>
-    );
+    </div>);
 }
 
 export default Header;
