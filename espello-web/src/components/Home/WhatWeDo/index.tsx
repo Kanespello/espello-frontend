9fb41c5e5b4f9bfc9da2539755017f22a5dcbcc8
Kanespello <162005722+Kanespello@users.noreply.github.com>
2024-04-21 16:14:28 +0530

    updating demo video

--- espello-web/src/components/Home/WhatWeDo/index.tsx
+++ espello-web/src/components/Home/WhatWeDo/index.tsx
@@ -6,15 +6,15 @@ const WhatWeDo = () => {
         <div className="box-2">
             <div className="box-2-media">
                 <div className="box-2-media-left">
-                            <iframe
-                    width="560"
-                    height="315"
-                    src={`https://www.youtube.com/embed/${videoId}`}
-                    title="YouTube video player"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    frameBorder="0"
-                    allowFullScreen
-                ></iframe>
+                    <iframe
+                        width="560"
+                        height="315"
+                        src={`https://www.youtube.com/embed/${videoId}`}
+                        title="YouTube video player"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        frameBorder="0"
+                        allowFullScreen
+                    />
                 </div>
                 <div className="box-2-media-right">
                     <div className="box-2-media-right-heading">
