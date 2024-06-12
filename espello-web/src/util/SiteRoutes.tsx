499e8ee02a4f4ed4cb839d4eab3f3dc63f41bc48
Kanespello <162005722+Kanespello@users.noreply.github.com>
2024-06-12 21:43:58 +0530

    updating blog URL

--- espello-web/src/util/SiteRoutes.tsx
+++ espello-web/src/util/SiteRoutes.tsx
@@ -5,5 +5,5 @@ export const PATH_CURRENT_SESSION_WITHOUT_SESSION_ID = '/session/current-session
 export const PATH_CURRENT_SESSION_WITH_SESSION_ID = `${PATH_CURRENT_SESSION_WITHOUT_SESSION_ID}/:sessionId`;
 export const PATH_CREATE_SESSION = '/session/create-session';
 export const PATH_USER_WAITLIST = '/user-waitlist';
-export const PATH_BLOGS = 'https://espello.notion.site/We-re-Revolutionising-HIRING-with-AI-de57804db5114ef2b033c6d5bf86ad52'
+export const PATH_BLOGS = 'https://espello.notion.site/Short-Reads-to-Learn-More-About-Us-c2c22d4053f043a7b3857441dd6bedd6?pvs=4'
 export const PATH_DEFAULT = '/*';
